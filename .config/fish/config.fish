6bb01a93a3cb1d6550eaaba07956dbf9c78c4ad3
GurmanDhmai05 <gurmandhami01@gmail.com>
2024-06-09 18:48:11 +0530

    Added Fish Shell config folder

---
+++ .config/fish/config.fish
@@ -0,0 +1,16 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+starship init fish | source
+set -g fish_greeting
+
+alias ":q" exit
+alias "v" "nvim"
+alias "ls" "lsd"
+
+# neofetch
+
+#qt5
+set -x QT_QPA_PLATFORMTHEME qt5ct
+
