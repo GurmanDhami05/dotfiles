a74ce2df266ea537c1df8ab61a6aef301f6504d6
GurmanDhami5 <gurmandhami01@gmail.com>
2026-08-19 08:37:28 +0530

    Nvim config changes

--- nvim/.config/nvim/lua/custom/keybinds.lua
+++ nvim/.config/nvim/lua/custom/keybinds.lua
@@ -6,8 +6,8 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
--- exit insert mode with jj
-vim.keymap.set('i', 'jj', '<Esc>', { noremap = false, desc = 'Exit Insert Mode' })
+-- exit insert mode with jk
+vim.keymap.set('i', 'jk', '<Esc>', { noremap = false, desc = 'Exit Insert Mode' })
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
@@ -22,3 +22,15 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 vim.keymap.set('n', '<leader>sv', '<C-w>v', { desc = 'Split Vertical' })
 vim.keymap.set('n', '<leader>sh', '<C-w>s', { desc = 'Split Horizontal' })
 vim.keymap.set('n', '<leader>sq', '<C-w>q', { desc = 'Close Split' })
+
+-- Move single lines in Normal mode
+vim.keymap.set('n', '<A-j>', ':m .+1<CR>==', { silent = true })
+vim.keymap.set('n', '<A-k>', ':m .-2<CR>==', { silent = true })
+
+-- Move single lines in Insert mode
+vim.keymap.set('i', '<A-j>', '<Esc>:m .+1<CR>==gi', { silent = true })
+vim.keymap.set('i', '<A-k>', '<Esc>:m .-2<CR>==gi', { silent = true })
+
+-- Move multiple selected lines in Visual mode
+vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv", { silent = true })
+vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv", { silent = true })
